f33c393a3c6ef4ff8af79e40b1dff8e3cb8c5337
antimad <udnwaege@gmail.com>
2021-03-17 23:19:23 -0700

    completed main simulation loop.

--- EOQ Wright-Fisher Model.py
+++ EOQ Wright-Fisher Model.py
@@ -16,17 +16,35 @@ mutation: Dictionary with the following k/v pairs
             Beneficial Effectiveness - Fitness value of the beneficial mutation will have.
             Dangerous - Number of detrimental mutations
             Dangerous Effectiveness - Fitness value of the Dangerous mutations
-            
+
+base_population: Dictionary with the following k/v pairs
+    The starting population may be randomize, but for simplicity, a population sequence array of just zero will be used.
+    **If randomize, be sure to remove diversity from the dictionary and calculate separately
+    
+            Sequence - sequence of zeros at the start of the population
+            Fitness - starting fitness of the population
 """
 s_length = 50
 pop_size = 1000
 generations = 100
 mutation = {
     "Rate": 0.001,
-    "Beneficial": np.random.randint(s_length),
+    "Beneficial": np.random.randint(s_length/2),
     "Beneficial Effectiveness": np.random.uniform(0, .4),
-    "Dangerous": np.random.randint(s_length),
+    "Dangerous": np.random.randint(s_length/2),
     "Dangerous Effectiveness": np.random.uniform(-.4, 0)
+}
+
+base_population = {
+    "Sequence": np.zeros(s_length),
+    "Fitness": 1,
+    "Diversity": np.array([pop_size])
+}
+
+growth_monitor = {
+    "Generational Sequences": [np.array([base_population["Sequence"]])],
+    "Count of Unique Generational Sequences": [base_population["Diversity"]],
+    "Population": [pop_size]
 }
 
 haplotypes = np.zeros(s_length)
@@ -49,7 +67,23 @@ chosen = np.random.multinomial(pop_size, pvals=init_probability)
 
 
 def select_mutant():
+    """
+    This function simply selects the number of individuals to mutate and nothing more
+
+    :return: The number of individuals to mutate.
+    """
     return np.random.binomial(pop_size, mutation["Rate"] * s_length)
+
+
+def select_site_to_mutate(mutated):
+    """
+    This function takes the number of mutants that will mutate an returns a numpy array of the sites that will mutate,
+    and randomly selects one of the possible s_length sites per mutant.
+
+    :param mutated: The number of people that will mutate
+    :return: Site that will mutate.
+    """
+    return np.random.randint(s_length, size=mutated)
 
 
 def mutate(selected_to_mutate):
@@ -59,24 +93,13 @@ def mutate(selected_to_mutate):
     :return: A dictionary of the new mutant sequences.
     """
 
-    def new_sequence():
-        """
-        Magic!
-
-        On a serious note. It seems that your clone function in your Species class creates a new sequence for each
-        mutation however, I can't figure out where or how it creates this new sequence. To me, I would start with a
-        blank slate, but your program creates a unique sequence per mutation.
-
-        :return: Magic!
-        """
-        return ["I'm really not sure yet..."]
-
     mutant_populous = {
         "Sequence": [],
         "Fitness": []
     }
-    for individual, site_location in enumerate(selected_to_mutate):
-        mutation_sequence = new_sequence()
+    for individual_sequence, site_location in enumerate(selected_to_mutate):
+        # Must explicitly call the .copy() function to assign a array to the variable
+        mutation_sequence = base_population["Sequence"].copy()
         mutation_sequence[site_location] += (1 - int(mutation_sequence[site_location]))
         fitness = 1 + np.sum(haplotypes * mutation_sequence)
 
@@ -84,3 +107,32 @@ def mutate(selected_to_mutate):
         mutant_populous["Fitness"].append(fitness)
 
     return mutant_populous
+
+
+for generation in range(generations):
+    """
+    At first the entire population will have the same sequence.
+    Based on the mutation rate, a few will be selected and mutations will take place.
+    
+    Those who will mutate will be chosen 
+    """
+    mutated_population = []
+    generational_monitor = {}
+
+    for population in range(len(growth_monitor["Population"])):
+        mutants = select_mutant()
+        mutation_sites = select_site_to_mutate(mutants)
+        mutation_seq_fit = mutate(mutation_sites)
+        growth_monitor["Population"][population] -= len(mutation_sites)
+
+        for n_mutants, mutant_sequence in enumerate(mutation_seq_fit["Sequence"]):
+            for previous, added_m_sequences in enumerate(mutated_population):
+                if np.array_equal(mutant_sequence, added_m_sequences):
+                    generational_monitor[added_m_sequences] += 1  # Given my mutation step, this will always be 1
+                    continue
+                if not np.array(mutant_sequence, added_m_sequences):
+                    generational_monitor[mutant_sequence] = 1
+                    mutated_population.append(mutant_sequence)
+
+    growth_monitor["Generational Sequences"].append(np.array(generational_monitor.keys()))
+    growth_monitor["Count of Unique Generational Sequences"].append(np.array(generational_monitor.values()))
