81893552d1dadb19b593612ca5eaa0e3da778f09
antimad <udnwaege@gmail.com>
2021-06-01 15:24:15 -0700

    used numpy to shorten loop

--- Inference.py
+++ Inference.py
@@ -1,100 +1,62 @@
 import numpy as np
-import pandas as pd
 
-Data = pd.read_json("Results.json")
+# Data = pd.read_json("Results.json").T
 mutation_rate = 0.001
 
+Data = np.load("Simulation_Data_2.csv", allow_pickle=True)
+
+
 Pop_Size = 1000  # Population size
-
-
-def fokker_planck(x_t, x_ij, s_i, mu, i, seq):
-    """
-
-    :param x_ij:
-    :param x_t: Site
-    :param s_i: Selection Coefficient
-    :param mu: Mutation Rate
-    :param i: Must be a numerical index of site
-    :param seq: Entire Sequence
-    :return: Results of Fokker-Planck equation
-    """
-    first = mu * (1 - (2 * x_t))
-    second = x_t * ((1 - x_t) * s_i)
-
-    third = 0
-    for idx, x_j in enumerate(seq["Mutant Allele"]):
-        if i != idx:
-            third += (x_ij - (x_t[i] * x_j)) * seq["Selection Coefficient"][idx]
-
-    return first + second + third
-
-
-def c_ij(x_t, x_ij,  i, j):
-    """
-
-    :param x_ij:
-    :param x_t: Results of the Fokker-Planck Equation
-    :param i: index, can be numerical or dictionary type. If dict, should be built dynamically
-    :param j: index, can be numerical or dictionary type. If dict, should be built dynamically
-    :return: Results of Covariance Matrix equation
-    """
-    if i == j:
-        i_eq_j = x_t[i] * (1 - x_t[i])
-        return i_eq_j
-
-    if i != j:
-        i_not_j = x_ij - (x_t[i] * x_t[j])
-        return i_not_j
-
-
-def identity_matrix(rows): # TODO: How do I determine the number of rows?
-    # rows = possible mutants, so sequence length.
-    return np.identity(n=rows)
-
 
 gamma = 1  # Works just as fine
 
 
-def s_inference(seq, generations):
-
-    #TODO: Switch site and generation
-    for i, generation in enumerate(generations):
-        pass
-
-    for idx, site in enumerate(seq):
-        site_selection_coefficient = 0 # extract based on datatype
-        mutation_rate = 0 # Most likely given and not stored here.
-        # Denominator
-        for i, generation in enumerate(generations):
-            delta_t = generation - generations[0]  # The distance between known generations
-            fo_pl = fokker_planck(x_t=site, s_i=site_selection_coefficient, mu=mutation_rate, i=idx, seq=seq, x_ij=0)
-            c_xt_k = c_ij(x_t=fo_pl, i=idx, j=idx, x_ij=0) # TODO: Is this being used correctly? Doubt...
-
-            part_1_a = delta_t * c_xt_k
-
-            part_1_b = gamma * identity_matrix(2)  # This could be a constant before starting
-
-            # Can use numpy module to compute x_i and x_ij
+def calc_xij(generation):
+    row_sum = generation.sum(axis=1)
+    x_ij_count = 0
+    for total in row_sum:
+        if total > 1:
+            x_ij_count += 1
+    return x_ij_count
 
 
-def inference(generation, site_s_coefficient):
-    for idx, sequence in enumerate(generation["Sequence"]):
-        freq = np.multiply(sequence, generation["Size"][idx])
-        complete_gen_sum = np.matmul(generation["Size"], generation["Sequence"])
-        delta_t = idx
-        for i, site in enumerate(complete_gen_sum):
-            x_i = site/Pop_Size
-            for j, site_j in enumerate(complete_gen_sum):
-                x_j = site_j / Pop_Size
-                if i == j:
-                    covariance = x_i * (1-x_i)
-                else:
-                    x_ij = 0
-                    covariance = x_ij - (x_i*x_j)
+def inference(generations):
+    for gen_num, sequences in enumerate(generations["Sequence"]):
+        sizes = generations["Size"][gen_num]
+        complete_gen_sum = (sequences.T * sizes).T
+        complete_gen_sum = sum(complete_gen_sum)
+        """
+        complete_gen_sum preserves the sequence information, and adds the number of individuals in a population that
+                        that have the same genetic sequence.
+                        
+        Using this information we will know the number of individuals where i=j. Where the sites of each individual is 
+        the same and possess a mutation. Then we take it and multiply it by (1 - itself).
+        """
+        x_ij = calc_xij(generation=sequences)
+        covariance = 0
+        gen_si = []
+        for abv_i_idx, x_i_sum in enumerate(complete_gen_sum):
+            delta_t = abv_i_idx + 1
+            covariance += x_i_sum * (1 - x_i_sum)  # First portion of the covariance
+            for abv_j_idx, x_j_sum in enumerate(complete_gen_sum):
+                covariance += x_ij - (x_i_sum*x_j_sum)  # Second portion of the covariance
 
-                part_1a = delta_t * covariance
-                part_1b = gamma * identity_matrix(2)
+            """
+            The entire block needs to stay in this index to track x_i_sum as it is being used.
+            """
+            covariance = covariance / Pop_Size
 
-                part_2a = 0
-                part_2b = mutation_rate*(delta_t*(1-(2*x_j)))
+            part_1a = delta_t * covariance
+            part_1b = 1
+            part_2a = x_i_sum - complete_gen_sum[0]
+            part_2b = mutation_rate*(delta_t*(1-(2*x_i_sum)))
 
+            s_i = (part_1a + part_1b) * (part_2a - part_2b)
+            gen_si.append(s_i)
+        inferred_selections.append(np.array(gen_si))
+
+
+inferred_selections = []
+inference(generations=Data)
+
+selection_coefficients = sum(inferred_selections)
