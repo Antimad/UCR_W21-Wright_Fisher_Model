809c905ad610d18b24e7d86039ff11edcfb829c3
antimad <udnwaege@gmail.com>
2021-06-01 20:40:40 -0700

    more changes, still no dice, might have made it worse

--- Inference.py
+++ Inference.py
@@ -11,20 +11,35 @@ Pop_Size = 1000  # Population size
 gamma = 1  # Works just as fine
 
 
-def calc_xij(generation):
-    row_sum = generation.sum(axis=1)
+def part_2b_func(site):
+    answer = mutation_rate * (1 - (2 * site))
+    return answer
+
+
+def calc_xij(generation, i, j):
+    ith_column = generation[:, i]
+    jth_column = generation[:, j]
+
+    column_sum = ith_column + jth_column
     x_ij_count = 0
-    for total in row_sum:
+    for total in column_sum:
         if total > 1:
             x_ij_count += 1
+
     return x_ij_count
 
 
 def inference(generations):
+    covariance_int = 0
+    part_2_helper = 0
+    part_2b_summation: int = 0
     for gen_num, sequences in enumerate(generations["Sequence"]):
+        delta_t = gen_num + 1
         sizes = generations["Size"][gen_num]
         complete_gen_sum = (sequences.T * sizes).T
         complete_gen_sum = sum(complete_gen_sum)
+        part_2_helper += complete_gen_sum
+
         """
         complete_gen_sum preserves the sequence information, and adds the number of individuals in a population that
                         that have the same genetic sequence.
@@ -32,28 +47,49 @@ def inference(generations):
         Using this information we will know the number of individuals where i=j. Where the sites of each individual is 
         the same and possess a mutation. Then we take it and multiply it by (1 - itself).
         """
-        x_ij = calc_xij(generation=sequences)
-        covariance = 0
-        gen_si = []
+        covariance_matrix = []
         for abv_i_idx, x_i_sum in enumerate(complete_gen_sum):
-            delta_t = abv_i_idx + 1
-            covariance += x_i_sum * (1 - x_i_sum)  # First portion of the covariance
+            covariance_diagonal = 0
+            covariance = 0
+            covariance_list = []
             for abv_j_idx, x_j_sum in enumerate(complete_gen_sum):
-                covariance += x_ij - (x_i_sum*x_j_sum)  # Second portion of the covariance
+                if abv_i_idx <= abv_j_idx:
+                    if abv_i_idx == abv_j_idx:
+                        covariance_diagonal = (x_i_sum * (1 - x_i_sum)) / Pop_Size
+                        covariance_list.append(covariance_diagonal)
+                    else:
+                        x_ij = calc_xij(generation=sequences, i=abv_i_idx, j=abv_j_idx)
+                        covariance = (x_ij - (x_i_sum*x_j_sum)) / Pop_Size  # Second portion of the covariance
+                        covariance_list.append(covariance)
+                else:
+                    covariance_list.append(0)
+            covariance_matrix.append(np.array(covariance_list))
+        covariance_matrix = np.array(covariance_matrix)
+        covariance_matrix = np.rot90(np.flipud(covariance_matrix)).T + covariance_matrix
 
-            """
-            The entire block needs to stay in this index to track x_i_sum as it is being used.
-            """
-            covariance = covariance / Pop_Size
+        """
+        Given that K tracks time. We must stay in the outermost loop for the rest of the equation
+        """
+        # covariance = covariance / Pop_Size
+        covariance_int += (covariance_matrix * delta_t).sum()
+        part_2b_summation += np.array(list(map(part_2b_func, complete_gen_sum)))
 
-            part_1a = delta_t * covariance
-            part_1b = 1
-            part_2a = x_i_sum - complete_gen_sum[0]
-            part_2b = mutation_rate*(delta_t*(1-(2*x_i_sum)))
+    seq_0 = generations["Sequence"][0]
+    size_0 = generations["Size"][0]
+    t_0 = sum((seq_0.T * size_0).T)
 
-            s_i = (part_1a + part_1b) * (part_2a - part_2b)
-            gen_si.append(s_i)
-        inferred_selections.append(np.array(gen_si))
+    seq_k = generations["Sequence"][-1]
+    size_k = generations["Size"][-1]
+    t_k = sum((seq_k.T * size_k).T)
+
+    for idx, x_i in enumerate(part_2b_summation):
+        part_1a = covariance_int
+        part_1b = 1
+        part_2a = t_k[idx] - t_0[idx]
+        part_2b = x_i
+
+        s_i = ((part_1a + part_1b)**-1) * (part_2a - part_2b)
+        inferred_selections.append(s_i)
 
 
 inferred_selections = []
